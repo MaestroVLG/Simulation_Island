bc7606ce69da7b81141f0e3734d41934b7cc7933
MaestroVLG <las-vegas1945@yandex.ru>
2026-02-23 01:03:04 +0300

    Implementation of the Location class. Description of adding and deleting entities

--- src/main/java/javarush_simulation/Location.java
+++ src/main/java/javarush_simulation/Location.java
@@ -1,7 +1,47 @@
 package javarush_simulation;
 
+import javarush_simulation.Entity.Animal;
+import javarush_simulation.Entity.Plant;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Location {
 
+    @Getter
 
+    private final List<Animal> animals = new CopyOnWriteArrayList<>();
+    private final List<Plant> plants = new CopyOnWriteArrayList<>();
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+        animal.setCurrentLocation(this);
+
+    }
+
+    public void removeAnimal(Animal animal){
+        animals.remove(animal);
+
+    }
+
+    public void addPlant(Plant plant){
+        plants.add(plant);
+
+    }
+
+    public Plant removePlant(){
+        synchronized (plants){
+            if (!plants.isEmpty()){
+                return plants.remove(plants.size() -1);
+
+            }
+            return null;
+        }
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+
+    }
 
 }
