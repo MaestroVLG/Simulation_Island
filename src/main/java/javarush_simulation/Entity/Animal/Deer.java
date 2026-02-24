6318dc059595438bbf133dc03166bc8b8234e18c
MaestroVLG <las-vegas1945@yandex.ru>
2026-02-24 16:40:36 +0300

    The Deer class has been created with a preliminary implementation

---
+++ src/main/java/javarush_simulation/Entity/Animal/Deer.java
@@ -0,0 +1,40 @@
+package javarush_simulation.Entity.Animal;
+
+import javarush_simulation.Entity.Plant;
+import javarush_simulation.model.Island;
+import javarush_simulation.model.Location;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Deer extends Animal{
+
+    private static final double WEIGHT = 170;
+    private static final double MAX_SATIETY = 50;
+
+    public Deer(){
+        super(WEIGHT, MAX_SATIETY);
+    }
+
+
+    @Override
+    public void eat(Location location) {
+        if(!isAlive){
+            return;
+        }
+
+        Plant plant = location.removePlant();
+        if(plant != null){
+            currentSatiety = Math.min(maxSatiety, currentSatiety + plant.getWeight(());
+            log.debug("Олень съел растение");
+        }
+
+    }
+
+    public void move(Island island, int currentX, int currentY){
+        // todo пока заглушка
+    }
+
+    public void reproduce(Location location){
+        // todo пока заглушка
+    }
+}
