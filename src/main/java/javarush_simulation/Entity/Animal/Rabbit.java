697690c7225c0fae4b03d852c00f132c0695a033
MaestroVLG <las-vegas1945@yandex.ru>
2026-02-25 00:26:13 +0300

    The Rabbit class has been created with a preliminary implementation

---
+++ src/main/java/javarush_simulation/Entity/Animal/Rabbit.java
@@ -0,0 +1,38 @@
+package javarush_simulation.Entity.Animal;
+
+import javarush_simulation.Entity.Plant;
+import javarush_simulation.model.Island;
+import javarush_simulation.model.Location;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Rabbit extends Animal{
+
+    private static final double WEIGHT = 2;
+    private static final double MAX_SATIETY = 1;
+
+    public Rabbit(){
+        super(WEIGHT, MAX_SATIETY);
+    }
+
+
+    @Override
+    public void eat(Location location) {
+        if(!isAlive){
+            return;
+        }
+        Plant plant = location.removePlant();
+        if (plant != null){
+            currentSatiety = Math.min(maxSatiety, currentSatiety + plant.getWeight());
+            log.debug("Кролик съел растение");
+        }
+    }
+
+    public void move(Island island, int currentX, int currentY){
+        // todo пока заглушка
+    }
+
+    public void reproduce(Location location){
+        // todo пока заглушка
+    }
+}
